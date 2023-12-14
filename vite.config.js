30784bdb838568bf8d9a96597ef768bd860b05a1
Wei Lun <weilun9320@hotmail.com>
2023-12-14 15:21:36 +0800

    Added deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/40-React-Vite-Bus-Arrival-App-API-CleanUp/"
 })
